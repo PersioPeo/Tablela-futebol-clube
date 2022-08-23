ed7f82fe2f054f5453f3c240409e1408c8fa2515
persio <persiopeo@hotmail.com>
2022-08-23 20:54:10 -0300

    feat: criando as migrations e users

---
+++ app/backend/src/database/models/users.model.ts
@@ -0,0 +1,38 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class users extends Model {}
+users.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER.UNSIGNED,
+    },
+    username: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    role: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    tableName: 'users',
+    modelName: 'users',
+    sequelize: db,
+    underscored: true,
+    timestamps: false,
+  },
+);
+export default users;
