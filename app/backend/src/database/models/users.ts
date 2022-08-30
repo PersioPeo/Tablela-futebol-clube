13cd6ba02ec72be07892d244a660e02249c81e2d
persio <persiopeo@hotmail.com>
2022-08-30 14:05:43 -0300

    feat: Re. configurando o models

---
+++ app/backend/src/database/models/users.ts
@@ -0,0 +1,37 @@
+import { DataTypes, Model, INTEGER } from 'sequelize';
+import db from '.';
+
+const users = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  role: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+};
+
+class Users extends Model {}
+Users.init(users, {
+  tableName: 'users',
+  modelName: 'users',
+  sequelize: db,
+  underscored: true,
+  timestamps: false,
+});
+export default Users;
