76e17a5ddc24fee0b8ff12996489440d93fa62aa
persio <persiopeo@hotmail.com>
2022-08-29 22:19:37 -0300

    feat: corrigindo arquivos para rodar test local

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -3,18 +3,14 @@ import LoginService from '../services/loginServices';
 import ErrorType from '../middleware/ErrorType';
 
 class LoginController {
-  private service: LoginService;
-
-  constructor() {
-    this.service = new LoginService();
-  }
+  private loginService = new LoginService();
 
   async login(req: Request, res: Response) {
     const { email, password } = req.body;
     if (!email || !password) {
       throw new ErrorType(400, 'All fields must be filled');
     }
-    const token = await this.service.login(req.body);
+    const token = await this.loginService.login(req.body);
     if (token === 'null') {
       throw new ErrorType(401, 'Incorrect email or password');
     }
@@ -22,12 +18,12 @@ class LoginController {
   }
 
   static async tokenValidation(req: Request, res: Response) {
-    const token = req.headers.authorization;
-    if (!token) {
+    const resultToken = req.headers.authorization;
+    if (!resultToken) {
       throw new ErrorType(401, 'Token invalid');
     }
-    const role = LoginService.validate(token);
-    return res.status(200).json({ role });
+    const resultRole = LoginService.validate(resultToken);
+    return res.status(200).json({ role: resultRole });
   }
 }
 
